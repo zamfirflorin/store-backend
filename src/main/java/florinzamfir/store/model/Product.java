db4408cf484905ff32790e7b3fbc26cd78e42f5d
florin.zamfir <florin.zamfir@raiffeisen.ch>
2024-05-12 18:08:09 +0300

    Create Product class

---
+++ src/main/java/florinzamfir/store/model/Product.java
@@ -0,0 +1,29 @@
+package florinzamfir.store.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "product")
+@Data
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private double price;
+
+    @Column
+    private String description;
+
+}
